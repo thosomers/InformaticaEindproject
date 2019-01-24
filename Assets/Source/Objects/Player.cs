44f3bb382ec01723dfbd199d5cff14ce9bb6e6ce
tomtrein <thomas.somers@zwijsencollege.org>
2019-01-24 17:15:34 +0100

    Started Code Documentation

--- Assets/Source/Objects/Player.cs
+++ Assets/Source/Objects/Player.cs
@@ -11,16 +11,45 @@ namespace Game.Objects
     public class Player
     {
 
+        /// <summary>
+        /// The <see cref="PlayerScript"/> that is linked to this <see cref="Player"/>. This Script runs all the Lua code of this Player.
+        /// </summary>
         public PlayerScript Script;
+        
+        
+        
+        /// <summary>
+        /// The other <see cref="Player"/> that is in the same game. See: <see cref="Setup"/>
+        /// </summary>
         public Player Enemy;
+        
+        
+        /// <summary>
+        /// The Lua-Proxy of this player
+        /// </summary>
         public LuaPlayer LuaPlayer;
+        
+        /// <summary>
+        /// A limited Lua-Proxy that is used for the Enemy <see cref="PlayerScript"/>.
+        /// </summary>
         public LuaEnemy LuaEnemy;
-
+        
+        /// <summary>
+        /// The <see cref="Base"/> object which this player controls. Instantiated by <see cref="PlayerScript.Setup"/>.
+        /// </summary>
         public Base Base;
+        
+        
+        /// <summary>
+        /// The list of <see cref="Robot"/> Objects controlled by this <see cref="Player"/>. This list is added to by <see cref="spawnRobot"/>.
+        /// </summary>
         public HashSet<Robot> Robots;
         
         
 
+        /// <summary>
+        /// The <see cref="SourceLoader"/> provides the Lua source as a string after calling: <see cref="SourceLoader.getLuaSource"/>
+        /// </summary>
         public SourceLoader Source;
 
         public Player(SourceLoader loader)
@@ -29,12 +58,20 @@ namespace Game.Objects
             this.Script = new PlayerScript(this);
         }
         
+        /// <summary>
+        /// A 2nd initializer for the Player object. This is run after the consytructors of this and the enemy player. Sets the Enemy for each player and sets  up the script. (<see cref="PlayerScript.Setup"/>).
+        /// </summary>
+        /// <param name="enemy">The other <see cref="Player"/> object in the game.</param>
         public void Setup(Player enemy)
         {
             this.Enemy = enemy;
             this.Script.Setup();
         }
 
+        /// <summary>
+        /// Spawns a new Robot for this player. Does NOT check requirements (See <see cref="LuaBase.spawnRobot"/>)
+        /// </summary>
+        /// <param name="func">The Function controlling the new Robot (<see cref="PlayerObject.Coroutine"/>)</param>
         public void spawnRobot(DynValue func)
         {
             var Robot = new Robot(this,func);
--- Assets/Source/Utils/PlayerObject.cs
+++ Assets/Source/Utils/PlayerObject.cs
@@ -8,14 +8,38 @@ namespace Game.Utils
 {
     public abstract class PlayerObject
     {
+        
+        
+        
+        
+        /// <summary>
+        /// The Lua coroutine that controls this specific object. The coroutine is paused for a number of ticks using: <see cref="runTick"/>.
+        /// </summary>
         private DynValue Coroutine;
+        
+        
+        
+        
+        
+        /// <summary>
+        /// The <see cref="Player"/> object which controls this object. The <see cref="Coroutine"/> on this object is also run with <see cref="PlayerScript"/> attached to this object.
+        /// </summary>
         public readonly Player Player;
 
+        
+        /// <summary>
+        /// The <see cref="LuaPlayerObject"/> which represents this object in Lua code. (This Object is used in Lua functions to act like a 'Proxy')
+        /// </summary>
         public LuaPlayerObject Lua { protected set; get; }
 
 
 
-
+        /// <summary>
+        /// The basic constructor for a PlayerObject: an object which is controlled by lua code.
+        /// </summary>
+        /// <param name="player">The <see cref="Player"/> (and therefore <see cref="PlayerScript"/>) which controls this object.</param> 
+        /// <param name="func">The Lua function which controls this object (using a while loop probably)</param> 
+        /// <exception cref="Exception">Exception if the Lua variable isn't a function.</exception>
         public PlayerObject(Player player, DynValue func)
         {
             this.Player = player;
@@ -31,6 +55,12 @@ namespace Game.Utils
             this.Player.Script.newObject(this, 0);
         }
         
+        
+        /// <summary>
+        /// The function which is called for every <see cref="PlayerObject"/> every tick of the game. This function runs the attached <see cref="Coroutine"/> till the next action and retuns the Tick-Cost of the executed action.
+        /// </summary>
+        /// <param name="script">The <see cref="PlayerScript"/> on which this <see cref="Coroutine"/> runs.</param>
+        /// <returns>the number of ticks to yield (Ticks to complete the action)</returns>
         public int runTick(PlayerScript script)
         {
             DynValue toSleep = Coroutine.Coroutine.Resume(this.Lua);
